fba5e56e09a4e3d9fbae87cd5509357ee88131b9
skedwards88 <skedwards88@github.com>
2021-09-26 08:13:35 -0700

    version

--- src/components/info.js
+++ src/components/info.js
@@ -4,7 +4,7 @@ export default function Info({ setCurrentDisplay }) {
   return (
     <div className="App">
       <div className="description">
-        {`Dragon Hero (beta 1.6)\n\na text adventure puzzle game\n\nThanks for play testing! We'd love to hear your feedback, especially about logical discrepancies or bugs.\n\nDesigned by Colin\nBuilt by Sarah\n\nWant more games?\nVisit `}
+        {`Dragon Hero (beta 1.7)\n\na text adventure puzzle game\n\nThanks for play testing! We'd love to hear your feedback, especially about logical discrepancies or bugs.\n\nDesigned by Colin\nBuilt by Sarah\n\nWant more games?\nVisit `}
         <a href="https://skedwards88.github.io/portfolio/">CnS Games</a>
       </div>
       <button className="close" onClick={() => setCurrentDisplay("location")}>
