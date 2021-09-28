eccedf6f4a445300eeec8e395896914b91373557
skedwards88 <skedwards88@gmail.com>
2021-09-28 01:51:49 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -321,7 +321,7 @@ define("./service-worker.js",['./workbox-718aa5be'], (function (workbox) { 'use 
     "revision": "20775fb0c0e64cb7eb59e7b233242d14"
   }, {
     "url": "bundle.js",
-    "revision": "04487e56aad693f0a0023e31c6a340fc"
+    "revision": "61e348647c7a55cfd23e5cd521174387"
   }, {
     "url": "d0c1e575e7452f03948e.svg",
     "revision": null
