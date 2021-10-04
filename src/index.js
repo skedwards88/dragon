735725c9028cadff51dd39dc6b70975a761cfbb1
skedwards88 <skedwards88@github.com>
2021-10-04 08:23:21 -0700

    prettier

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 
 if ("serviceWorker" in navigator) {
-  console.log('HOSTNAME')
-  console.log(location.hostname)
-  const path = (location.hostname === "localhost") ? "/service-worker.js" : "/dragon/service-worker.js";
-  const scope = (location.hostname === "localhost") ? "" : '/dragon/';
+  console.log("HOSTNAME");
+  console.log(location.hostname);
+  const path =
+    location.hostname === "localhost"
+      ? "/service-worker.js"
+      : "/dragon/service-worker.js";
+  const scope = location.hostname === "localhost" ? "" : "/dragon/";
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register(path, {scope: scope})
+      .register(path, { scope: scope })
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
