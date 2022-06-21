5c2e1c9b07a631b35d10882f875d10fa8f52867f
skedwards88 <skedwards88@github.com>
2022-06-21 06:41:42 -0700

    make dev service worker experience better

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,13 @@
 import React from "react";
+import packageJson from "../../package.json";
 
 export default function Info({ setCurrentDisplay }) {
   return (
     <div className="App">
       <div className="description">
-        {`Dragon Hero (beta 1.9)\n\na text adventure puzzle game\n\nThanks for play testing! We'd love to hear your feedback, especially about logical discrepancies or bugs.\n\nDesigned by Colin\nBuilt by Sarah\n\nWant more games?\nVisit `}
+        {`Dragon Hero (beta ${packageJson.version})\n\na text adventure puzzle game\n\nThanks for play testing! We'd love to hear your feedback, especially about logical discrepancies or bugs.`}
+        {<hr></hr>}
+        {`Story by Colin\nBuilt by Sarah\n\nWant more games?\nVisit `}
         <a href="https://skedwards88.github.io/portfolio/">CnS Games</a>
       </div>
       <button className="close" onClick={() => setCurrentDisplay("location")}>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
-if ("serviceWorker" in navigator) {
-  console.log("HOSTNAME");
-  console.log(location.hostname);
+if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
   const path =
     location.hostname === "localhost"
       ? "/service-worker.js"
