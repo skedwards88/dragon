23261f603dfd94154d4beee5918d72af68647414
skedwards88 <skedwards88@github.com>
2022-11-13 07:46:45 -0800

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,23 @@ const WorkboxPlugin = require("workbox-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = (env, argv) => {
-
-  if (argv.mode === 'development') {
-    console.log('RUNNING IN DEV MODE. Service worker will not generate.')
+  if (argv.mode === "development") {
+    console.log("RUNNING IN DEV MODE. Service worker will not generate.");
   } else {
-    console.log('RUNNING IN NON-DEV MODE. Service worker will generate.')
+    console.log("RUNNING IN NON-DEV MODE. Service worker will generate.");
   }
 
   const htmlPlugin = new HtmlWebpackPlugin({
     // Need to use template because need 'root' div for react injection. templateContent doesn't play nice with title, so just use a template file instead.
     template: "./src/index.html",
-  })
+  });
 
   const serviceWorkerPlugin = new WorkboxPlugin.GenerateSW({
     // these options encourage the ServiceWorkers to get in there fast
     // and not allow any straggling "old" SWs to hang around
     clientsClaim: true,
     skipWaiting: true,
-  })
+  });
 
   const faviconPlugin = new FaviconsWebpackPlugin({
     logo: "./src/images/favicon.png",
@@ -44,10 +43,12 @@ module.exports = (env, argv) => {
         yandex: false,
       },
     },
-  })
+  });
 
-  const plugins = argv.mode === 'development' ? [htmlPlugin, faviconPlugin] : [htmlPlugin, faviconPlugin, serviceWorkerPlugin]
-
+  const plugins =
+    argv.mode === "development"
+      ? [htmlPlugin, faviconPlugin]
+      : [htmlPlugin, faviconPlugin, serviceWorkerPlugin];
 
   return {
     entry: "./src/index.js",
@@ -82,5 +83,5 @@ module.exports = (env, argv) => {
       port: 4001,
     },
     plugins: plugins,
-  }
+  };
 };
